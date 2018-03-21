b89c06224ee7a8321b2ded344d30cf58b63c2fea
Keilin Olsen <keilin.olsen@anz.com>
2018-03-21 13:24:57 +1100

    update eslint processor to include custom weights

--- analysis.js
+++ analysis.js
@@ -24,7 +24,56 @@ const requireOptional = (fname, _default) => (
   }
 )(path.join(process.cwd(), fname))
 
+const customFormatter = (results, config) => {
+  const ERROR_WEIGHT = config.weights.error
+  const WARNING_WEIGHT = config.weights.warning
 
+  results = results || []
+
+  const weightLookup = config.weights.rules.reduce( (accum, entry) => {
+    entry.rules.forEach( rule => {
+      accum[rule] = entry.weight
+    })
+
+    return accum
+  }, {})
+
+  const convertDebt = obj => {
+    // console.log(obj)
+    let debtCount = 0
+
+    let mappedMessages = obj.messages.map( message => {
+        let ruleId = message.ruleId
+        let fallbackScore = (message.severity === 2 ? ERROR_WEIGHT : WARNING_WEIGHT)
+        let score = weightLookup[message.ruleId] || fallbackScore
+
+        debtCount += score
+        message.debt = score
+    })
+
+    return {
+      debt: debtCount
+    }
+  }
+
+  let summary = results.results.reduce( (accum, result) => {
+    let detail = Object.assign({}, result, convertDebt(result))
+
+    accum.debt += detail.debt
+    accum.fileDetails.push(detail)
+
+    return accum
+  }, {
+    fileDetails: [],
+    totals: {
+      errorCount: results.errorCount,
+      warningCount: results.warningCount,
+      debt: 0
+    }
+  })
+
+  return summary
+}
 
 const eslintProcess = (() => {
   const eslint = require('eslint')
@@ -32,14 +81,12 @@ const eslintProcess = (() => {
 
   return (fileList, globalConfig) => {
     const config = globalConfig.eslint
-    const ERROR_WEIGHT = config.weights.error
-    const WARNING_WEIGHT = config.weights.warning
+
+    console.log(config.weights)
+    console.log('\n\n\n\n###########')
 
     let cwd = process.cwd()
     // console.log({config, cwd, __dirname})
-    const convertDebt = obj => ({
-      debt: obj.errorCount * ERROR_WEIGHT + obj.warningCount * WARNING_WEIGHT
-    })
 
     let cli = new CLIEngine(
       Object.assign({
@@ -50,52 +97,52 @@ const eslintProcess = (() => {
     // let eslintResults = cli.executeOnFiles(['./*.js'])
     let eslintResults = cli.executeOnFiles(fileList)
 
-    let results = Object.assign({}, {
-      fileDetails: eslintResults.results.map( result => Object.assign({}, result, convertDebt(result)) ),
-      totals: {
-        errorCount: eslintResults.errorCount,
-        warningCount: eslintResults.warningCount,
-        debt: convertDebt(eslintResults).debt
-      }
-    })
+    // let results = Object.assign({}, {
+    //   fileDetails: eslintResults.results.map( result => Object.assign({}, result, convertDebt(result)) ),
+    //   totals: {
+    //     errorCount: eslintResults.errorCount,
+    //     warningCount: eslintResults.warningCount,
+    //     debt: convertDebt(eslintResults).debt
+    //   }
+    // })
 
-    return results
+    return customFormatter(eslintResults, config)
   }
 })()
 
 
-const acornProcess = (() => {
-  const acorn = require('acorn-jsx')
-
-  return (fileList, globalConfig) => {
-    const config = globalConfig.acorn
-    const globOptions = globalConfig.glob
-
-    const processFile = filename => {
-      const source = fs.readFileSync(filename, 'utf8')
-      const ast = acorn.parse(source, {
-
-      })
-
-      // ACORN RULES HERE
-
-      return ast
-    }
-
-    const allResults = fileList.map( filename => {
-      let result = processFile(filename)
-    })
-    console.log(`matched ${fileList.length} files`)
-    let results = {
-      // fileDetails: allResults.map( result => Object.assign({}, result, convertDebt(result)) ),
-      totals: {
-        debt: 0
-      }
-    }
-
-    return results
-  }
-})()
+// const acornProcess = (() => {
+//   const acorn = require('acorn-jsx')
+//
+//   return (fileList, globalConfig) => {
+//     const config = globalConfig.acorn
+//     const globOptions = globalConfig.glob
+//
+//     const processFile = filename => {
+//       const source = fs.readFileSync(filename, 'utf8')
+//       const ast = acorn.parse(source, {
+//
+//       })
+//
+//       // ACORN RULES HERE
+//
+//       return ast
+//     }
+//
+//     const allResults = fileList.map( filename => {
+//       let result = processFile(filename)
+//     })
+//     console.log(`matched ${fileList.length} files`)
+//     let results = {
+//       // fileDetails: allResults.map( result => Object.assign({}, result, convertDebt(result)) ),
+//       totals: {
+//         debt: 0
+//       }
+//     }
+//
+//     return results
+//   }
+// })()
 
 ;(function main() {
   check_valid_node_version()
@@ -107,19 +154,20 @@ const acornProcess = (() => {
   }
 
   const userConfig = requireOptional('./analysis.config.js', {message:'no user config'})
+  console.log(userConfig.eslint.weights)
   const config = Object.assign({}, defaultConfig, userConfig)
 
-  const fileList = globby.sync(config.targets, config.globOptions)
+  // const fileList = globby.sync(config.targets, config.globOptions)
 
-  let acorn = acornProcess(fileList, config)
+  // let acorn = acornProcess(fileList, config)
 
-  console.log(acorn)
+  // console.log(acorn)
 
-  let esl = eslintProcess(fileList, config)
+  let esl = eslintProcess(config.targets, config)
 
-  let total = esl.totals.debt + acorn.totals.debt
+  console.dir(esl, {color: true})
+  let total = esl.totals.debt
   console.log(`
-Acorn debt: ${acorn.totals.debt} ()
 Eslint debt: ${esl.totals.debt} (${esl.totals.errorCount} errors & ${esl.totals.warningCount} warnings)
 ---
 TOTAL DEBT: ${total}
