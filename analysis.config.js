668a066cfaa034955c14befa36f5904416803133
Keilin Olsen <keilin.olsen@heathwallace.com>
2018-03-21 13:38:56 +1100

    Fixed merge errors
    fix more merge errors

--- analysis.config.js
+++ analysis.config.js
@@ -8,14 +8,119 @@
 'use strict'
 
 const targets = [
-  // 'sbos-ui/server',
-  // 'sbos-ui/src',
-  // 'sbos-ui/stories',
-  // 'sbos-ui/tests',
-  // 'libs',
-  'tests'
+  'sbos-ui/server',
+  'sbos-ui/src',
+  'libs',
 ]
 
+const severity = {
+  style: 1,
+  practices: 5,
+  med: 10,
+  high: 20,
+  critical: 100
+}
+
 module.exports = {
-  targets
+  targets,
+
+  eslint: {
+    /**
+     *
+     * Rule overides sent directly to eslint
+     *
+     */
+    rules: {
+      /**
+       * Stylistic preferences a la ADS
+       */
+      'quotes': 1,
+      'jsx-quotes': ['error', 'prefer-single'],
+
+
+      /**
+       * Hardcore issues
+       */
+      // cyclomatic complexity
+      'complexity': ['error', {max:5}],
+      'max-nested-callbacks': ['error', {max: 5}],
+      'max-params': ['error', {max: 10}],
+      'max-statements': ['error', {max: 15}],
+      'max-depth': ['error', {max: 4}],
+      // maximum line length
+      'max-len': ['error', {
+        code: 80
+      }],
+      'eqeqeq': ['error'],
+      'no-extra-bind': ['error'],
+      'no-global-assign': ['error'],
+      'no-undef': ['error'],
+      'no-unused-vars': ['error'],
+
+      'no-const-assign': ['error']
+    },
+
+    /**
+     *
+     * Weights used to calculate a debt metric
+     *
+     */
+    weights: {
+      // default weights used for warnings and errors
+      warning: severity.practices,
+      error: severity.high,
+
+      // rule-specific weights
+      rules: [
+        //
+        // low severity
+        //
+        {
+          weight: severity.style,
+          rules: [
+            'quotes', 'jsx-quotes',
+            'max-len'
+          ]
+        },
+
+        //
+        // medium severity
+        //
+        {
+          weight: severity.practices,
+          rules: [
+            'max-nested-callbacks', 'max-params',
+            'max-depth', 'no-extra-bind'
+          ]
+        },
+        {
+          weight: severity.med,
+          rules: [
+            'complexity', 'max-statements',
+            'no-unused-vars',
+            'no-undef'
+          ]
+        },
+
+        //
+        // high severity
+        //
+        {
+          weight: severity.high,
+          rules: [
+            'no-global-assign', 'no-const-assign'
+          ]
+        },
+        {
+          weight: severity.critical,
+          rules: [
+            'eqeqeq',
+          ]
+        }
+      ]
+    }
+  },
+
+  acorn: {
+  }
 }
--- analysis.js
+++ analysis.js
@@ -5,6 +5,7 @@ const fs = require('fs')
 
 const commander = require('commander')
 const chalk = require('chalk')
+const globby = require('globby')
 
 const program = commander
   .option('-v,--verbose', 'verbose logging')
@@ -23,77 +24,159 @@ const check_valid_node_version = () => {
   }
 }
 
+
 const requireOptional = (fname, _default) => (
   (fname) => {
-    // console.log(fname)
+    console.log(fname)
     return fs.existsSync(fname) ? require(fname) : _default
   }
 )(path.join(process.cwd(), fname))
 
-const eslintProcess = config => {
-  const ERROR_WEIGHT = 100
-  const WARNING_WEIGHT = 10
+const customFormatter = (results, config) => {
+  const ERROR_WEIGHT = config.weights.error
+  const WARNING_WEIGHT = config.weights.warning
 
-  let cwd = process.cwd()
-  // console.log({config, cwd, __dirname})
-  const convertDebt = obj => ({
-    debt: obj.errorCount * ERROR_WEIGHT + obj.warningCount * WARNING_WEIGHT
+  results = results || []
+
+  const weightLookup = config.weights.rules.reduce( (accum, entry) => {
+    entry.rules.forEach( rule => {
+      accum[rule] = entry.weight
+    })
+
+    return accum
+  }, {})
+
+  const convertDebt = obj => {
+    // console.log(obj)
+    let debtCount = 0
+
+    let mappedMessages = obj.messages.map( message => {
+        let ruleId = message.ruleId
+        let fallbackScore = (message.severity === 2 ? ERROR_WEIGHT : WARNING_WEIGHT)
+        let score = weightLookup[message.ruleId] || fallbackScore
+
+        debtCount += score
+        message.debt = score
+    })
+
+    return {
+      debt: debtCount
+    }
+  }
+
+  let summary = results.results.reduce( (accum, result) => {
+    let detail = Object.assign({}, result, convertDebt(result))
+
+    accum.debt += detail.debt
+    accum.fileDetails.push(detail)
+
+    return accum
+  }, {
+    fileDetails: [],
+    totals: {
+      errorCount: results.errorCount,
+      warningCount: results.warningCount,
+      debt: 0
+    }
   })
 
-  let CLIEngine = require('eslint').CLIEngine
-  let cli = new CLIEngine({
-    // useEslintrc: true
-  })
-
-  // let eslintResults = cli.executeOnFiles(['./*.js'])
-  let eslintResults = cli.executeOnFiles(config.targets)
-  // console.log(eslintResults)
-
-  let results = Object.assign({}, convertDebt(eslintResults), {
-    fileDetails: eslintResults.results.map( result => Object.assign({}, result, convertDebt(result)) ),
-    errorCount: eslintResults.errorCount,
-    warningCount: eslintResults.warningCount
-  })
-
-  return results
+  return summary
 }
 
-(function main() {
+const eslintProcess = (() => {
+  const eslint = require('eslint')
+  const CLIEngine = eslint.CLIEngine
+
+  return (fileList, globalConfig) => {
+    const config = globalConfig.eslint
+
+    console.log(config.weights)
+    console.log('\n\n\n\n###########')
+
+    let cwd = process.cwd()
+    // console.log({config, cwd, __dirname})
+
+    let cli = new CLIEngine(
+      Object.assign({
+        useEslintrc: true
+      }, config)
+    )
+
+    // let eslintResults = cli.executeOnFiles(['./*.js'])
+    let eslintResults = cli.executeOnFiles(fileList)
+
+    // let results = Object.assign({}, {
+    //   fileDetails: eslintResults.results.map( result => Object.assign({}, result, convertDebt(result)) ),
+    //   totals: {
+    //     errorCount: eslintResults.errorCount,
+    //     warningCount: eslintResults.warningCount,
+    //     debt: convertDebt(eslintResults).debt
+    //   }
+    // })
+
+    return customFormatter(eslintResults, config)
+  }
+})()
+
+
+// const acornProcess = (() => {
+//   const acorn = require('acorn-jsx')
+//
+//   return (fileList, globalConfig) => {
+//     const config = globalConfig.acorn
+//     const globOptions = globalConfig.glob
+//
+//     const processFile = filename => {
+//       const source = fs.readFileSync(filename, 'utf8')
+//       const ast = acorn.parse(source, {
+//
+//       })
+//
+//       // ACORN RULES HERE
+//
+//       return ast
+//     }
+//
+//     const allResults = fileList.map( filename => {
+//       let result = processFile(filename)
+//     })
+//     console.log(`matched ${fileList.length} files`)
+//     let results = {
+//       // fileDetails: allResults.map( result => Object.assign({}, result, convertDebt(result)) ),
+//       totals: {
+//         debt: 0
+//       }
+//     }
+//
+//     return results
+//   }
+// })()
+
+;(function main() {
   check_valid_node_version()
 
   const defaultConfig = {
-
+    globOptions: {
+      gitignore: true,
+    }
   }
+
   const userConfig = requireOptional('./analysis.config.js', {message:'no user config'})
+  console.log(userConfig.eslint.weights)
   const config = Object.assign({}, defaultConfig, userConfig)
 
-  let esl = eslintProcess(config)
-  // console.log(esl)
+  // const fileList = globby.sync(config.targets, config.globOptions)
 
-  if (program.full) {
-    console.log('+-- full log\n|')
-    let lastPath = ''
-    const report = esl.fileDetails.map( result => {
-      // console.log(result)
-      let {filePath, errorCount, warningCount, debt} = result
-      let fileDir = path.dirname(filePath)
-      let reportLine = ''
+  // let acorn = acornProcess(fileList, config)
 
-      if (fileDir !== lastPath) {
-        reportLine += `+ ${chalk.white.bold(fileDir)}:\n`
-        lastPath = fileDir
-      }
-      reportLine += `|-   ${path.basename(filePath)} ${chalk.yellow.bold(debt)} (${chalk.red(errorCount)} ${chalk.yellow(warningCount)})\n`
+  // console.log(acorn)
 
-      return reportLine
-    })
+  let esl = eslintProcess(config.targets, config)
 
-    console.log(report.join(''))
-  }
-
-  let total = esl.debt
+  console.dir(esl, {color: true})
+  let total = esl.totals.debt
   console.log(`
-Eslint debt: ${esl.debt} (${esl.errorCount} errors & ${esl.warningCount} warnings)
+Eslint debt: ${esl.totals.debt} (${esl.totals.errorCount} errors & ${esl.totals.warningCount} warnings)
 ---
 TOTAL DEBT: ${total}
 ---
